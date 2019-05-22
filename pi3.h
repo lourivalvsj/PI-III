5732921f2bf507b1448abead7638f9b0e531a8a2
Romildo Jr <fis.romildojr@gmail.com>
2019-05-22 16:13:35 -0300

    Inicio criação cabeçalho das funções

---
+++ pi3.h
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+void InserirFuncionario();
+
+void CadastrarCartaoPonto();
+
+void GerarEspelho();
+
+void GerarHolerite();
+
+void ExibirFuncionario();
+
+void ExibirEspelho();
+
+void ExibirHolerite();
